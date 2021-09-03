5597d33ae2b447644e324645587dbfcb95b51246
nazzz328 <nazzz328@gmail.com>
2021-09-03 15:47:27 +0200

    First commit

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+
+    class Students
+    {
+        public int Id {get;set;}
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+        public int Course {get;set;}
+        public double AverageGrade {get;set;}
+        public int Scholarship {get;set;}
+        
+    }
+}
